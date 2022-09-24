945b86cb082da08c3c3aaed27dc1358bdaa44e75
Geovann Gunawan <63842114+amm0nition@users.noreply.github.com>
2022-09-24 18:29:50 +0700

    random shit again
    - reformatted game.java
    - commented menusong.java cuz useless shit

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,4 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 import java.awt.*;
 
 public class Game {
@@ -6,22 +6,22 @@ public class Game {
     JFrame window;
     Container con;
 
-    public static void main(String[] args) {
-
-        new Game();
-    }
-
-    public Game(){
+    public Game() {
 
         window = new JFrame();
         window.setTitle("KORIDO");
-        window.setSize(800,600);
+        window.setSize(800, 600);
         window.setResizable(false);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.getContentPane().setBackground(Color.BLACK);
         window.setLayout(null);
         window.setVisible(true);
         con = window.getContentPane();
+    }
+
+    public static void main(String[] args) {
+
+        new Game();
     }
 
 
