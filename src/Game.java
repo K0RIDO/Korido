8082eee89b24808c66d4f34528c37beb1ed0af11
Geovann Gunawan <63842114+amm0nition@users.noreply.github.com>
2022-10-01 12:31:34 +0700

    Title Screen Added
    Title Screen and Start Button added.
    Removed unused java classes.

--- src/Game.java
+++ src/Game.java
@@ -5,6 +5,11 @@ public class Game {
 
     JFrame window;
     Container con;
+    JPanel titleNamePanel, startButtonPanel;
+    JLabel titleNameLabel;
+    Font titleFont = new Font("MS Gothic", Font.PLAIN, 96);
+    Font startButtonFont = new Font("Times New Roman", Font.PLAIN, 32);
+    JButton startButton;
 
     public Game() {
 
@@ -18,8 +23,31 @@ public class Game {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.getContentPane().setBackground(Color.BLACK);
         window.setLayout(null);
-        window.setVisible(true);
         con = window.getContentPane();
+
+        titleNamePanel = new JPanel();
+        titleNamePanel.setBounds(100, 100, 600, 150);
+        titleNamePanel.setBackground(Color.BLACK);
+        titleNameLabel = new JLabel("コリドー");
+        titleNameLabel.setForeground(Color.WHITE);
+        titleNameLabel.setFont(titleFont);
+
+        startButtonPanel = new JPanel();
+        startButtonPanel.setBounds(300, 400, 200, 100);
+        startButtonPanel.setBackground(Color.BLACK);
+
+        startButton = new JButton("START");
+        startButton.setBackground(Color.BLACK);
+        startButton.setForeground(Color.WHITE);
+        startButton.setFont(startButtonFont);
+
+        titleNamePanel.add(titleNameLabel);
+        startButtonPanel.add(startButton);
+
+        window.add(titleNamePanel);
+        window.add(startButtonPanel);
+
+        window.setVisible(true);
     }
 
     public static void main(String[] args) {
