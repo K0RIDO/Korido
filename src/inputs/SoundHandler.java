95a597f4a81dc38a599a114c1bc596963f2e3913
Geovann Gunawan <63842114+amm0nition@users.noreply.github.com>
2023-06-07 20:58:16 +0700

    Update SoundHandler.java

--- src/inputs/SoundHandler.java
+++ src/inputs/SoundHandler.java
@@ -9,14 +9,14 @@ import static javax.sound.sampled.Clip.LOOP_CONTINUOUSLY;
 public class SoundHandler {
 
     public static void RunMusic(String path) {
-        try{
+        try {
             AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(path));
             Clip clip = AudioSystem.getClip();
             clip.open(inputStream);
             clip.loop(LOOP_CONTINUOUSLY);
         } catch (UnsupportedAudioFileException e) {
             e.printStackTrace();
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         } catch (LineUnavailableException e) {
             e.printStackTrace();
