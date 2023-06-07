7d0634f9de31097f1248a5331798487cab77b8ea
Geovann Gunawan <63842114+amm0nition@users.noreply.github.com>
2023-06-07 20:46:33 +0700

    Create SoundHandler.java

---
+++ src/inputs/SoundHandler.java
@@ -0,0 +1,25 @@
+package inputs;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+public class SoundHandler {
+
+    public static void RunMusic(String path) {
+        try{
+            AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(path));
+            Clip clip = AudioSystem.getClip();
+            clip.open(inputStream);
+            clip.loop(0);
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
